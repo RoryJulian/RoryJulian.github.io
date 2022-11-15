05db581588d7feb780bddf9e18e7f087e1c19927
RoryJulian <x22171983@student.ncirl.ie>
2022-11-15 19:41:35 +0000

    Commit changes.
    Updating from CV to Rock, Paper, Scissors.

---
+++ script.js
@@ -0,0 +1,17 @@
+alert ("Welcome to the Rock, Paper, Scissors game. Input your selection below and see if you can beat the computer!");
+
+function check(){
+var choice = document.getElementById("formAnswer").value;
+if(choice=="Rock"){
+    document.getElementById("gameDiv").innerHTML="Uh oh, you lose! The computer chose Paper.";
+    }   
+
+    else if(choice=="Paper"){
+    document.getElementById("gameDiv").innerHTML="It's a tie! The computer chose Paper.";
+    }
+    else if(choice=="Scissors"){
+    document.getElementById("gameDiv").innerHTML="Congratulations, you won! The computer chose Paper.";
+    }
+    else {alert("invalid input, please either input Rock, Paper or Scissors!")
+    }
+}
